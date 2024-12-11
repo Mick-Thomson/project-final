39aeebe37c34a3ba891e3baeeab37e0708707b30
Mick Thomson <mechanical_dildo@mail.ru>
2024-12-11 19:46:47 +0300

    Tests have been written for all public ProfileRestController methods

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,64 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.common.util.JsonUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.javarush.jira.common.util.JsonUtil.writeValue;
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.USER_PROFILE_TO;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+    private static final String URL = ProfileRestController.REST_URL;
 
+    @Test
+    public void shouldReturn401ForUnauthorizedUser() throws Exception {
+        perform(MockMvcRequestBuilders.get(URL))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    public void shouldReturn200GetAccessAdminProfileAndReturnJsonTypeContent() throws Exception {
+        perform(MockMvcRequestBuilders.get(URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.*", hasSize(4)))
+                .andDo(print());
+    }
+
+    @Test
+    public void shouldReturn401ForUnauthorizedUserUpdatingProfileWithJsonTypeContent() throws Exception {
+        perform(MockMvcRequestBuilders.put(URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = GUEST_MAIL)
+    void shouldReturn204ForGuestUpdating() throws Exception {
+        perform(MockMvcRequestBuilders.put(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(USER_PROFILE_TO)))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void shouldReturn422WhenSendingInvalidDataToServer() throws Exception {
+        perform(MockMvcRequestBuilders.put(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getInvalidTo())))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
 }
